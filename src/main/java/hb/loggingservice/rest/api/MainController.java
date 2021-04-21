3d9e33e4e60eb95b5da3c07b8bcb92172278ad16
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-21 13:18:53 +0200

    Add Swagger configuration and annotations for documentation

--- src/main/java/hb/loggingservice/rest/api/MainController.java
+++ src/main/java/hb/loggingservice/rest/api/MainController.java
@@ -1,12 +1,18 @@
 package hb.loggingservice.rest.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "Main")
 public class MainController {
 
+    @ApiOperation(value = "Sample controller to say hello")
+    @ApiResponse(code = 200, message = "Returns hello world")
     @GetMapping
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello World!");
