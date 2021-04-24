e6f0db46e711bc28fc7e8e161c7abad6d89b36b8
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-24 11:20:42 +0200

    Modify MainController to display state of the service with information

--- src/main/java/hb/loggingservice/rest/api/MainController.java
+++ src/main/java/hb/loggingservice/rest/api/MainController.java
@@ -1,20 +1,27 @@
 package hb.loggingservice.rest.api;
 
+import hb.loggingservice.model.ServiceStateModel;
+import hb.loggingservice.service.ServiceStateService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "Main")
+@RequiredArgsConstructor
 public class MainController {
 
-    @ApiOperation(value = "Sample controller to say hello")
-    @ApiResponse(code = 200, message = "Returns hello world")
+    private final ServiceStateService stateService;
+
+    @ApiOperation(value = "Displays state of the service with information")
+    @ApiResponse(code = 200, message = "Returns state of the service",
+        response = ServiceStateModel.class)
     @GetMapping
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello World!");
+    public ResponseEntity<ServiceStateModel> displayServiceState() {
+        return ResponseEntity.ok(stateService.getServiceState());
     }
 }
--- src/test/java/hb/loggingservice/rest/api/MainControllerTest.java
+++ src/test/java/hb/loggingservice/rest/api/MainControllerTest.java
@@ -1,15 +1,22 @@
 package hb.loggingservice.rest.api;
 
+import hb.loggingservice.TestUtil;
+import hb.loggingservice.repository.ConfigurationRepository;
+import hb.loggingservice.repository.LogRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,19 +24,56 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MainControllerTest {
 
     private static final String BASE_URL = "/";
-
+    @MockBean
+    ConfigurationRepository configurationRepository;
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private LogRepository logRepository;
 
     @Test
-    @DisplayName("Should return Hello World!")
-    void sayHello() throws Exception {
-        //Given
-        final String expectedResult = "Hello World!";
+    @DisplayName("Successfully returns service state with default information values")
+    void shouldDisplayServiceStateWithDefaultValues() throws Exception {
         //When and expect
+        int expectedNumberOfLogs = 0;
+        int expectedMaxAge = 0;
+        int expectedTotalNumberOfMessages = 0;
+        int expectedAverageNumberOfMessagesPerLog = 0;
+
         mockMvc.perform(get(BASE_URL))
             .andExpect(status().is2xxSuccessful())
             .andDo(print())
-            .andExpect(content().string(expectedResult));
+            .andExpect(jsonPath("$.numberOfLogs").value(expectedNumberOfLogs))
+            .andExpect(jsonPath("$.maxAge").value(expectedMaxAge))
+            .andExpect(jsonPath("$.totalNumberOfMessages").value(expectedTotalNumberOfMessages))
+            .andExpect(jsonPath("$.averageNumberOfMessagesPerLog").value(expectedAverageNumberOfMessagesPerLog));
+    }
+
+    @Test
+    @DisplayName("Successfully returns service state with information values")
+    void shouldDisplayServiceStateWithValues() throws Exception {
+        //Given
+        final String configName = "MAX_AGE";
+        int maxAgeValue = 10;
+        when(logRepository.findAll()).thenReturn(
+            List.of(TestUtil.createMockLogWithFourMessages(),
+                TestUtil.createMockLog("test message 1"),
+                TestUtil.createMockLog("test message 2"))
+        );
+        when(configurationRepository.findAll())
+            .thenReturn(List.of(TestUtil.createMockConfiguration(configName, maxAgeValue)));
+        //When and expect
+        int expectedNumberOfLogs = 3;
+        int expectedMaxAge = 10;
+        int expectedTotalNumberOfMessages = 6;
+        double expectedAverageNumberOfMessagesPerLog = 2;
+
+        mockMvc.perform(get(BASE_URL))
+            .andExpect(status().is2xxSuccessful())
+            .andDo(print())
+            .andExpect(jsonPath("$.numberOfLogs").value(expectedNumberOfLogs))
+            .andExpect(jsonPath("$.maxAge").value(expectedMaxAge))
+            .andExpect(jsonPath("$.totalNumberOfMessages").value(expectedTotalNumberOfMessages))
+            .andExpect(jsonPath("$.averageNumberOfMessagesPerLog").value(expectedAverageNumberOfMessagesPerLog));
     }
 }
