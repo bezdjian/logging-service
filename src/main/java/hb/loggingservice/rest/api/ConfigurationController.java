cd64770628fa99f2ccfd13cfd4824575e0d54cbd
Harout Bezdjian <harout.bezdjian@sbab.se>
2021-04-24 19:41:13 +0200

    Modify ConfigurationController to return response model with message and status instead of empty response.

--- src/main/java/hb/loggingservice/rest/api/ConfigurationController.java
+++ src/main/java/hb/loggingservice/rest/api/ConfigurationController.java
@@ -1,12 +1,14 @@
 package hb.loggingservice.rest.api;
 
 import hb.loggingservice.model.ConfigurationNames;
+import hb.loggingservice.model.ResponseModel;
 import hb.loggingservice.service.ConfigurationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,18 +22,27 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "Configuration")
 public class ConfigurationController {
 
+    private static final String SUCCESSFULLY_CREATED_MSG = "Configuration has been successfully saved for %s";
     private final ConfigurationService configurationService;
 
     @ApiOperation(value = "Create a configuration",
         notes = "- MAX_AGE: Numbers in seconds to set visibility on messages in logs")
-    @ApiResponse(code = 200, message = "Configuration is created")
+    @ApiResponse(code = 200, message = "Configuration is created",
+        response = ResponseModel.class)
     @PutMapping("/{value}")
-    public ResponseEntity<String> createConfiguration(
+    public ResponseEntity<ResponseModel> createConfiguration(
         @ApiParam(value = "The value of the configuration", example = "10")
         @PathVariable int value,
         @ApiParam(value = "Name of the configuration", example = "MAX_AGE")
         @RequestParam ConfigurationNames configurationName) {
         configurationService.saveConfiguration(configurationName.name(), value);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(ResponseModel.builder()
+            .message(formatResponseMessage(configurationName))
+            .status(HttpStatus.OK.value())
+            .build());
+    }
+
+    private String formatResponseMessage(ConfigurationNames configurationName) {
+        return String.format(SUCCESSFULLY_CREATED_MSG, configurationName.name());
     }
 }
--- src/test/java/hb/loggingservice/rest/api/ConfigurationControllerTest.java
+++ src/test/java/hb/loggingservice/rest/api/ConfigurationControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,6 +18,7 @@ class ConfigurationControllerTest {
     private static final String BASE_URL = "/configure";
     private static final String CONFIG_PARAM_VALUE = "MAX_AGE";
     private static final String CONFIG_PARAM = "configurationName";
+    private static final String SUCCESSFULLY_CREATED_MSG = "Configuration has been successfully saved for %s";
 
     @Autowired
     private MockMvc mockMvc;
@@ -27,9 +29,11 @@ class ConfigurationControllerTest {
         //Given
         int value = 10;
         //When & expect
+        String expectedResponseMessage = String.format(SUCCESSFULLY_CREATED_MSG, CONFIG_PARAM_VALUE);
         mockMvc.perform(put(BASE_URL + "/" + value)
             .queryParam(CONFIG_PARAM, CONFIG_PARAM_VALUE))
-            .andExpect(status().is2xxSuccessful());
+            .andExpect(status().is2xxSuccessful())
+            .andExpect(jsonPath("$.message").value(expectedResponseMessage));
     }
 
     @Test
